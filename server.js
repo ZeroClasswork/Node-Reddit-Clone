0c5e7ed2bb6f8ccca4dbfb7554ee6cdd4e06373f
atleastzero <megan.obryan52@gmail.com>
2021-01-30 22:20:07 -0800

    Created basic templates

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'layout', handlebars: allowInsecurePrototypeAccess(Handlebars) }))
+
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {

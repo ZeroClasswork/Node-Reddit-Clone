f6950badae29aeebce94419ee2c5a1dd562e6642
atleastzero <megan.obryan52@gmail.com>
2021-02-01 14:14:08 -0800

    Users can view a list of posts

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Add after body parser initialization!
 app.use(expressValidator());
 
+
 app.engine('handlebars', exphbs({ defaultLayout: 'layout', handlebars: allowInsecurePrototypeAccess(Handlebars) }))
 
 app.set('view engine', 'handlebars')
@@ -22,10 +23,18 @@ app.set('view engine', 'handlebars')
 // Set db
 require('./data/reddit-db');
 
+const Post = require('./models/post');
+
 require('./controllers/posts.js')(app);
 
 app.get('/', (req, res) => {
-  res.render('home')
+  Post.find({}).lean()
+    .then(posts => {
+      res.render('posts-index', { posts });
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
 })
 
 app.get('/posts/new', (req, res) => {

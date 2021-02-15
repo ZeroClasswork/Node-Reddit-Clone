14344d23a7c2944ad449244b3bad11e83db25fa8
atleastzero <megan.obryan52@gmail.com>
2021-02-15 12:09:55 -0800

    Users can now vote on posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,9 +21,12 @@ module.exports = (app) => {
   app.post('/posts/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL
     if (req.user) {
-      var post = new Post(req.body);
+      var post = new Post(req.body)
 
       post.author = req.user._id
+      post.upVotes = []
+      post.downVotes = []
+      post.voteScore = 0
 
       // SAVE INSTANCE OF POST MODEL TO DB
       post
@@ -66,5 +69,41 @@ module.exports = (app) => {
       .catch((err) => {
         console.log(err)
       })
+  })
+
+  app.put("/posts/:id/vote-up", function(req, res) {
+    if (req.user) {
+      Post.findById(req.params.id)
+        .then((post) => {
+          post.upVotes.push(req.user._id)
+          post.voteScore = post.voteScore + 1
+          post.save()
+
+          res.status(200)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    } else {
+      return res.status(401)
+    }
+  })
+
+  app.put("/posts/:id/vote-down", function(req, res) {
+    if (req.user) {
+      Post.findById(req.params.id)
+        .then((post) => {
+          post.downVotes.push(req.user._id)
+          post.voteScore = post.voteScore - 1
+          post.save()
+
+          res.status(200)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    } else {
+      return res.status(401)
+    }
   })
 };
--- models/post.js
+++ models/post.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const Populate = require("../util/autopopulate")
 
-
 const PostSchema = new Schema({
   subreddit: { type: String, required: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  author : { type: Schema.Types.ObjectId, ref: "User", required: true }
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  upVotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downVotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  voteScore: {type: Number},
 })
 
 PostSchema
